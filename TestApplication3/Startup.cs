20b8d3456ab085d495f254d045961c92b729f348
sxr207 <swapna.ramasamy@sherwin.com>
2017-04-28 14:55:36 -0400

    TestApp3

---
+++ TestApplication3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApplication3.Startup))]
+namespace TestApplication3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
